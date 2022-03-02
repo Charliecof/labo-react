d52f4f91369357bb9cce5567b6dc1cd758fb02c5
Charliecof <a01328800@itesm.mx>
2022-03-02 10:25:04 -0600

    updated  addition Component closes #1

--- src/components/addition/Addition.js
+++ src/components/addition/Addition.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Addition() {
     return (
-        <div style={{marginBottom:'15px', fontWeight:'bold'}}>
+        <div style={{marginBottom:'15px', fontWeight:'bold' }}>
             (+)
         </div>
     )
